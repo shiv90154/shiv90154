b4aba8087168aea6f8007ead74090cadbd6f6226
Shiv Kumar <140259236+shiv90154@users.noreply.github.com>
2024-07-20 16:38:01 +0530

    Add files via upload

---
+++ javascript/question_solving/day_4.js
@@ -0,0 +1,7 @@
+//use strict mode
+"use strict";
+
+
+// in use strict mode me varible ko define krnaa hai isme var, let const , likhna pdta  hai without that you can'nt define a variable
+// in use strict we cant not delet an variavble 
+// wwe can'nt pass same parameters in same function  
